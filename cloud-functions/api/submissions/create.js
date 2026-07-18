d0924ffc56434688d721352b74e02da8ec0b49d0
srhovo <xiaxue200576@gmail.com>
2026-07-18 22:04:35 +0800

    热修4B.2：候选提交读取Node运行时环境变量

--- cloud-functions/api/submissions/create.js
+++ cloud-functions/api/submissions/create.js
@@ -1,5 +1,6 @@
+import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
 import { handleSubmissionCreateRequest } from '../../../src/server/preview_write_http_v1.js';
 
 export default async function onRequest(context) {
-  return handleSubmissionCreateRequest(context);
+  return handleSubmissionCreateRequest(resolveCloudFunctionContext(context));
 }
