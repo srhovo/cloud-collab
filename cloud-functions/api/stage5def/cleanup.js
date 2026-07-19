51af9fd871e37cf1a1f8cfde1422b400a131ef5a
srhovo <xiaxue200576@gmail.com>
2026-07-20 04:24:59 +0800

    fix(stage5def): correct cleanup wrapper import depth

--- cloud-functions/api/stage5def/cleanup.js
+++ cloud-functions/api/stage5def/cleanup.js
@@ -1,5 +1,5 @@
 import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
-import { handleStage5defCleanupRequest } from '../../../../src/server/stage5def_cleanup_http_v1.js';
+import { handleStage5defCleanupRequest } from '../../../src/server/stage5def_cleanup_http_v1.js';
 
 export default async function onRequest(context) {
   return handleStage5defCleanupRequest(resolveCloudFunctionContext(context));
