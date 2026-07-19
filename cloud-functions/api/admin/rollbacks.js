e688f69b3dd0d32da2203806fce99424bea37a83
srhovo <xiaxue200576@gmail.com>
2026-07-20 02:56:37 +0800

    feat(stage5e): add admin rollback list route

---
+++ cloud-functions/api/admin/rollbacks.js
@@ -0,0 +1,6 @@
+import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
+import { handleAdminRollbackListRequest } from '../../../src/server/admin_rollback_http_v1.js';
+
+export default async function onRequest(context) {
+  return handleAdminRollbackListRequest(resolveCloudFunctionContext(context));
+}
