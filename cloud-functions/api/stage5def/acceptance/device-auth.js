7d1a552e8dfdb577c912705d7c4a43fe33e31f5a
srhovo <xiaxue200576@gmail.com>
2026-07-20 04:24:40 +0800

    fix(stage5def): correct device-auth wrapper import depth

--- cloud-functions/api/stage5def/acceptance/device-auth.js
+++ cloud-functions/api/stage5def/acceptance/device-auth.js
@@ -1,5 +1,5 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleStage5defDeviceAuthRequest } from '../../../../../src/server/stage5def_acceptance_http_v1.js';
+import { handleStage5defDeviceAuthRequest } from '../../../../src/server/stage5def_acceptance_http_v1.js';
 
 export default async function onRequest(context) {
   return handleStage5defDeviceAuthRequest(resolveCloudFunctionContext(context));
