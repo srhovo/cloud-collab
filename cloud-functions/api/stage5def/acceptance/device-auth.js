89fb5e12bc6bd0f56c0b025076a12883bd478f81
srhovo <xiaxue200576@gmail.com>
2026-07-20 04:19:37 +0800

    feat(stage5def): add blocked-device auth route wrapper

---
+++ cloud-functions/api/stage5def/acceptance/device-auth.js
@@ -0,0 +1,6 @@
+import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
+import { handleStage5defDeviceAuthRequest } from '../../../../../src/server/stage5def_acceptance_http_v1.js';
+
+export default async function onRequest(context) {
+  return handleStage5defDeviceAuthRequest(resolveCloudFunctionContext(context));
+}
