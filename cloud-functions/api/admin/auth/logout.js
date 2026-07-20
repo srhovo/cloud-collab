428dc9172b110a8319fc99018e9dabf1506d3153
srhovo <xiaxue200576@gmail.com>
2026-07-21 07:50:16 +0800

    feat(stage7s): dispatch admin logout by runtime mode

--- cloud-functions/api/admin/auth/logout.js
+++ cloud-functions/api/admin/auth/logout.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminLogoutRequest } from '../../../../src/server/admin_auth_http_v1.js';
+import { handleAdminLogoutByMode } from '../../../../src/server/admin_auth_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminLogoutRequest(resolveCloudFunctionContext(context));
+  return handleAdminLogoutByMode(resolveCloudFunctionContext(context));
 }
