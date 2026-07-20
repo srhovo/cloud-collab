4acb266f601f58c2ee8fea8296a7817895833876
srhovo <xiaxue200576@gmail.com>
2026-07-21 07:54:10 +0800

    fix(stage7s): retain stage5a login handler compatibility name

--- cloud-functions/api/admin/auth/login.js
+++ cloud-functions/api/admin/auth/login.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminLoginByMode } from '../../../../src/server/admin_auth_mode_dispatch_v1.js';
+import { handleAdminLoginByMode as handleAdminLoginRequest } from '../../../../src/server/admin_auth_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminLoginByMode(resolveCloudFunctionContext(context));
+  return handleAdminLoginRequest(resolveCloudFunctionContext(context));
 }
