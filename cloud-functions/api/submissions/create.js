b7f8e56833803adfc83c568ecb9f6c6b9b69538a
srhovo <xiaxue200576@gmail.com>
2026-07-21 07:17:31 +0800

    fix(stage7p): retain historical submission handler audit alias

--- cloud-functions/api/submissions/create.js
+++ cloud-functions/api/submissions/create.js
@@ -1,6 +1,8 @@
 import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
-import { dispatchSubmissionCreateRequest } from '../../../src/server/write_mode_dispatch_v1.js';
+import {
+  dispatchSubmissionCreateRequest as handleSubmissionCreateRequest,
+} from '../../../src/server/write_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return dispatchSubmissionCreateRequest(resolveCloudFunctionContext(context));
+  return handleSubmissionCreateRequest(resolveCloudFunctionContext(context));
 }
