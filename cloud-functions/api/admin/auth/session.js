ae66227be1dc7a0c3fe6830723e18e855889fb9a
srhovo <xiaxue200576@gmail.com>
2026-07-21 07:55:03 +0800

    [阶段7S] 管理员会话路由改用生产预览分发器

--- cloud-functions/api/admin/auth/session.js
+++ cloud-functions/api/admin/auth/session.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminSessionRequest } from '../../../../src/server/admin_auth_http_v1.js';
+import { dispatchAdminSessionRequest } from '../../../../src/server/admin_auth_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminSessionRequest(resolveCloudFunctionContext(context));
+  return dispatchAdminSessionRequest(resolveCloudFunctionContext(context));
 }
