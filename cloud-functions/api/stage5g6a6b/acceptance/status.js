7004bd8a9ec2c8a1ab6077a7e1fc9601cc7224c4
srhovo <xiaxue200576@gmail.com>
2026-07-20 16:25:14 +0800

    feat(acceptance): add status route

---
+++ cloud-functions/api/stage5g6a6b/acceptance/status.js
@@ -0,0 +1,6 @@
+import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
+import { handleStage5g6a6bStatusRequest } from '../../../../src/server/stage5g6a6b_acceptance_http_v1.js';
+
+export default async function onRequest(context) {
+  return handleStage5g6a6bStatusRequest(resolveCloudFunctionContext(context));
+}
