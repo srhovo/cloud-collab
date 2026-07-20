ad3a81470b7c983e26dc0f33518b564e38daf508
srhovo <xiaxue200576@gmail.com>
2026-07-21 07:54:27 +0800

    fix(stage7s): retain stage5a session handler compatibility name

--- cloud-functions/api/admin/auth/session.js
+++ cloud-functions/api/admin/auth/session.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminSessionByMode } from '../../../../src/server/admin_auth_mode_dispatch_v1.js';
+import { handleAdminSessionByMode as handleAdminSessionRequest } from '../../../../src/server/admin_auth_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminSessionByMode(resolveCloudFunctionContext(context));
+  return handleAdminSessionRequest(resolveCloudFunctionContext(context));
 }
