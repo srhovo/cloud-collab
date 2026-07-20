33320e5eb3b3b1ff5141fd7b39abe95546682748
srhovo <xiaxue200576@gmail.com>
2026-07-21 07:12:43 +0800

    feat(stage7p): dispatch submission creation to production or preview

--- cloud-functions/api/submissions/create.js
+++ cloud-functions/api/submissions/create.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
-import { handleSubmissionCreateRequest } from '../../../src/server/preview_write_http_v1.js';
+import { dispatchSubmissionCreateRequest } from '../../../src/server/write_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleSubmissionCreateRequest(resolveCloudFunctionContext(context));
+  return dispatchSubmissionCreateRequest(resolveCloudFunctionContext(context));
 }
