cf51cf31984a16e1ebb976ccf433bd820b2e097c
srhovo <xiaxue200576@gmail.com>
2026-07-20 16:37:29 +0800

    feat: add acceptance public version route

---
+++ cloud-functions/api/stage5g6a6b/acceptance/public-version.js
@@ -0,0 +1,6 @@
+import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
+import { handleStage5g6a6bPublicVersionRequest } from '../../../../src/server/stage5g6a6b_acceptance_proxy_http_v1.js';
+
+export default async function onRequest(context) {
+  return handleStage5g6a6bPublicVersionRequest(resolveCloudFunctionContext(context));
+}
