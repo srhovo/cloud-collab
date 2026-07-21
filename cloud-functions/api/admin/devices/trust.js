c581136584c9f24cd096e2c9a05f04c1ce05e715
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:59:25 +0800

    [阶段7W] 设为可信入口接入生产模式分发

--- cloud-functions/api/admin/devices/trust.js
+++ cloud-functions/api/admin/devices/trust.js
@@ -1,5 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminDeviceTrustRequest } from '../../../../src/server/device_governance_http_v1.js';
+// Stage5D compatibility anchor: device_governance_http_v1
+import { handleAdminDeviceTrustByMode as handleAdminDeviceTrustRequest } from '../../../../src/server/admin_device_governance_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminDeviceTrustRequest(resolveCloudFunctionContext(context));
