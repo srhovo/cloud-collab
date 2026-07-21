42b12f0d32285610226fd4c9e1d995250efd999b
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:54:47 +0800

    [阶段7W] 撤销可信改用模式分发

--- cloud-functions/api/admin/devices/revoke-trust.js
+++ cloud-functions/api/admin/devices/revoke-trust.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminDeviceRevokeTrustRequest } from '../../../../src/server/device_governance_http_v1.js';
+import { handleAdminDeviceRevokeTrustByMode } from '../../../../src/server/device_governance_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminDeviceRevokeTrustRequest(resolveCloudFunctionContext(context));
+  return handleAdminDeviceRevokeTrustByMode(resolveCloudFunctionContext(context));
 }
