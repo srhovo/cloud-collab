2733b6d3561031888ccf935148b92572b95cd509
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:05:32 +0800

    feat(stage7t): dispatch exact review queue by admin mode

--- cloud-functions/api/admin/reviews.js
+++ cloud-functions/api/admin/reviews.js
@@ -1,5 +1,5 @@
 import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
-import { handleAdminReviewQueueRequest } from '../../../src/server/admin_review_http_v1.js';
+import { handleAdminExactReviewQueueByMode as handleAdminReviewQueueRequest } from '../../../src/server/admin_review_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminReviewQueueRequest(resolveCloudFunctionContext(context));
