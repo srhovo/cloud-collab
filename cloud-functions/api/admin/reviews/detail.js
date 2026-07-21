f1e9e6086a3ebcaf1b52c9a99e30cb3d3bf65686
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:19:20 +0800

    [阶段7U] 精确价格审核详情改用模式分发

--- cloud-functions/api/admin/reviews/detail.js
+++ cloud-functions/api/admin/reviews/detail.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminReviewDetailRequest } from '../../../../src/server/admin_review_http_v1.js';
+import { handleAdminExactReviewDetailByMode } from '../../../../src/server/admin_review_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminReviewDetailRequest(resolveCloudFunctionContext(context));
+  return handleAdminExactReviewDetailByMode(resolveCloudFunctionContext(context));
 }
