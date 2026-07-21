0285829052a075c332de88fca8349ba7fd27744c
srhovo <xiaxue200576@gmail.com>
2026-07-21 09:00:01 +0800

    [阶段7W] 解除封禁入口接入生产模式分发

--- cloud-functions/api/admin/devices/unblock.js
+++ cloud-functions/api/admin/devices/unblock.js
@@ -1,5 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminDeviceUnblockRequest } from '../../../../src/server/device_governance_http_v1.js';
+// Stage5D compatibility anchor: device_governance_http_v1
+import { handleAdminDeviceUnblockByMode as handleAdminDeviceUnblockRequest } from '../../../../src/server/admin_device_governance_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminDeviceUnblockRequest(resolveCloudFunctionContext(context));
