9e07ac07922ff95240ab5adf8c7051f1a94b18c6
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:19:34 +0800

    [阶段7U] 普通审核队列改用模式分发

--- cloud-functions/api/admin/ordinary-reviews.js
+++ cloud-functions/api/admin/ordinary-reviews.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
-import { handleAdminOrdinaryReviewQueueRequest } from '../../../src/server/admin_ordinary_review_http_v1.js';
+import { handleAdminOrdinaryReviewQueueByMode } from '../../../src/server/admin_review_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminOrdinaryReviewQueueRequest(resolveCloudFunctionContext(context));
+  return handleAdminOrdinaryReviewQueueByMode(resolveCloudFunctionContext(context));
 }
