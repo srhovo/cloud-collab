20d53e108c794e6f695467845687ee875b31046b
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:06:59 +0800

    feat(stage7t): dispatch ordinary review rejection by admin mode

--- cloud-functions/api/admin/ordinary-reviews/reject.js
+++ cloud-functions/api/admin/ordinary-reviews/reject.js
@@ -1,5 +1,5 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminOrdinaryReviewRejectRequest } from '../../../../src/server/admin_ordinary_review_mutation_http_v1.js';
+import { handleAdminOrdinaryReviewRejectByMode as handleAdminOrdinaryReviewRejectRequest } from '../../../../src/server/admin_review_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminOrdinaryReviewRejectRequest(resolveCloudFunctionContext(context));
