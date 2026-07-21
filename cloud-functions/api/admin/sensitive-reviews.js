83c17f975c81ef3a089e2a52d3b0a6ae1f7f5d5a
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:31:34 +0800

    [阶段7V] 敏感审核队列改用模式分发

--- cloud-functions/api/admin/sensitive-reviews.js
+++ cloud-functions/api/admin/sensitive-reviews.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
-import { handleAdminSensitiveReviewQueueRequest } from '../../../src/server/admin_sensitive_review_http_v1.js';
+import { handleAdminSensitiveReviewQueueByMode } from '../../../src/server/admin_sensitive_review_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminSensitiveReviewQueueRequest(resolveCloudFunctionContext(context));
+  return handleAdminSensitiveReviewQueueByMode(resolveCloudFunctionContext(context));
 }
