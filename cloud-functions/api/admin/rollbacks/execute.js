abcc95bfef9b7248040f78858ab05e2565994e64
srhovo <xiaxue200576@gmail.com>
2026-07-21 09:18:56 +0800

    [阶段7Y] 回滚执行入口接入生产模式分发

--- cloud-functions/api/admin/rollbacks/execute.js
+++ cloud-functions/api/admin/rollbacks/execute.js
@@ -1,5 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminRollbackExecuteRequest } from '../../../../src/server/admin_rollback_http_v1.js';
+// Stage5E compatibility anchor: admin_rollback_http_v1
+import { handleAdminRollbackExecuteByMode as handleAdminRollbackExecuteRequest } from '../../../../src/server/admin_rollback_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminRollbackExecuteRequest(resolveCloudFunctionContext(context));
