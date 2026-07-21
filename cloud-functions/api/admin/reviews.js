54ce77636d0557b7cca6f74649f8e39bb87fd0a5
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:19:02 +0800

    [阶段7U] 精确价格审核队列改用模式分发

--- cloud-functions/api/admin/reviews.js
+++ cloud-functions/api/admin/reviews.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
-import { handleAdminReviewQueueRequest } from '../../../src/server/admin_review_http_v1.js';
+import { handleAdminExactReviewQueueByMode } from '../../../src/server/admin_review_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminReviewQueueRequest(resolveCloudFunctionContext(context));
+  return handleAdminExactReviewQueueByMode(resolveCloudFunctionContext(context));
 }
