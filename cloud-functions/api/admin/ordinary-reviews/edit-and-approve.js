dfecd25105bb6e115fa6e99ce70b0cd6f1b8790e
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:07:11 +0800

    feat(stage7t): dispatch ordinary review edit approval by admin mode

--- cloud-functions/api/admin/ordinary-reviews/edit-and-approve.js
+++ cloud-functions/api/admin/ordinary-reviews/edit-and-approve.js
@@ -1,5 +1,5 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminOrdinaryReviewEditAndApproveRequest } from '../../../../src/server/admin_ordinary_review_mutation_http_v1.js';
+import { handleAdminOrdinaryReviewEditAndApproveByMode as handleAdminOrdinaryReviewEditAndApproveRequest } from '../../../../src/server/admin_review_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminOrdinaryReviewEditAndApproveRequest(resolveCloudFunctionContext(context));
