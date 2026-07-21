9e968aeedcc2b55b4212a6897c97bc3754fcd649
srhovo <xiaxue200576@gmail.com>
2026-07-21 09:03:10 +0800

    [阶段7X] 回滚候选列表路由接入模式分发

--- cloud-functions/api/admin/rollbacks.js
+++ cloud-functions/api/admin/rollbacks.js
@@ -1,5 +1,7 @@
 import { resolveCloudFunctionContext } from '../../_shared/runtime_env.js';
-import { handleAdminRollbackListRequest } from '../../../src/server/admin_rollback_http_v1.js';
+import {
+  handleAdminRollbackListByMode as handleAdminRollbackListRequest,
+} from '../../../src/server/admin_rollback_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminRollbackListRequest(resolveCloudFunctionContext(context));
