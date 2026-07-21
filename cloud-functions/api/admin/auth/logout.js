c3efa93163757b1311ec8808f04994b3e7beaf94
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:01:36 +0800

    [阶段7T] 管理员退出路由改用生产预览分发器

--- cloud-functions/api/admin/auth/logout.js
+++ cloud-functions/api/admin/auth/logout.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminLogoutRequest } from '../../../../src/server/admin_auth_http_v1.js';
+import { dispatchAdminLogoutRequest } from '../../../../src/server/admin_auth_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminLogoutRequest(resolveCloudFunctionContext(context));
+  return dispatchAdminLogoutRequest(resolveCloudFunctionContext(context));
 }
