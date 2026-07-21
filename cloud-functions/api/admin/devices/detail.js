470901ffa0559a52db5a55005b77f624b7dd6b33
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:54:22 +0800

    [阶段7W] 设备详情改用模式分发

--- cloud-functions/api/admin/devices/detail.js
+++ cloud-functions/api/admin/devices/detail.js
@@ -1,6 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminDeviceDetailRequest } from '../../../../src/server/device_governance_http_v1.js';
+import { handleAdminDeviceDetailByMode } from '../../../../src/server/device_governance_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
-  return handleAdminDeviceDetailRequest(resolveCloudFunctionContext(context));
+  return handleAdminDeviceDetailByMode(resolveCloudFunctionContext(context));
 }
