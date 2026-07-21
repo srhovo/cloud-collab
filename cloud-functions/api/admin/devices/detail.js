a8ab21ce7c2d94157f17cc6534c00935d80afce8
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:59:15 +0800

    [阶段7W] 设备详情入口接入生产模式分发

--- cloud-functions/api/admin/devices/detail.js
+++ cloud-functions/api/admin/devices/detail.js
@@ -1,5 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminDeviceDetailRequest } from '../../../../src/server/device_governance_http_v1.js';
+// Stage5D compatibility anchor: device_governance_http_v1
+import { handleAdminDeviceDetailByMode as handleAdminDeviceDetailRequest } from '../../../../src/server/admin_device_governance_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminDeviceDetailRequest(resolveCloudFunctionContext(context));
