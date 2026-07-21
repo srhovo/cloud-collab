9a27e8bda7369c71b148f48407ee0c55b3f03fa3
srhovo <xiaxue200576@gmail.com>
2026-07-21 09:34:52 +0800

    [阶段8A] 导出摘要入口接入生产与预览分发

--- cloud-functions/api/admin/exports/summary.js
+++ cloud-functions/api/admin/exports/summary.js
@@ -1,5 +1,6 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminExportSummaryRequest } from '../../../../src/server/admin_export_http_v1.js';
+// Stage5F compatibility anchor: admin_export_http_v1
+import { handleAdminExportSummaryByMode as handleAdminExportSummaryRequest } from '../../../../src/server/admin_export_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminExportSummaryRequest(resolveCloudFunctionContext(context));
