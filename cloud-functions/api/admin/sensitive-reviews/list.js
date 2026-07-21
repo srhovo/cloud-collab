892a70411acdd471968089eb6e4cec12b367b890
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:10:17 +0800

    feat(stage7t): dispatch sensitive review list with historical handler alias

---
+++ cloud-functions/api/admin/sensitive-reviews/list.js
@@ -0,0 +1,3 @@
+import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
+import { dispatchSensitiveReviewListRequest as handleSensitiveReviewListRequest } from '../../../../src/server/admin_sensitive_review_mode_dispatch_v1.js';
+export default async function onRequest(context){return handleSensitiveReviewListRequest(resolveCloudFunctionContext(context));}
