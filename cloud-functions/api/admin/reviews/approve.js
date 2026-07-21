e08f723538e50cc699c1d4c79cdfba0ed2f2e4d2
srhovo <xiaxue200576@gmail.com>
2026-07-21 08:05:56 +0800

    feat(stage7t): dispatch exact review approval by admin mode

--- cloud-functions/api/admin/reviews/approve.js
+++ cloud-functions/api/admin/reviews/approve.js
@@ -1,5 +1,5 @@
 import { resolveCloudFunctionContext } from '../../../_shared/runtime_env.js';
-import { handleAdminReviewApproveRequest } from '../../../../src/server/admin_review_mutation_http_v1.js';
+import { handleAdminExactReviewApproveByMode as handleAdminReviewApproveRequest } from '../../../../src/server/admin_review_mode_dispatch_v1.js';
 
 export default async function onRequest(context) {
   return handleAdminReviewApproveRequest(resolveCloudFunctionContext(context));
